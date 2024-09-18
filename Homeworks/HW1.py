6a7e0d56cffaa0202d9186c28961bf7ab608f843
xBob45 <xmorav45@vutbr.cz>
2024-09-18 11:00:32 +0200

    Homework1 solution.

---
+++ Homeworks/HW1.py
@@ -0,0 +1,40 @@
+from math import gcd
+
+
+class DecisionalDiffieHellman():
+    def __init__(self, modulo : int, generator : int, j_instance : tuple):
+        self.modulo = modulo
+        self.generator = generator
+        self.j_instance = j_instance
+
+        self.computation()
+    
+    def computation(self):
+
+        # 1) We calculate order of the group (number of its elements) -> φ({n} and establish pool of values from which we'll be drawing x and y
+        available_values = []
+        
+        for number in range(1,self.modulo):
+            if gcd(number, self.modulo) == 1: # If that's the case a number and the modulo are mutualy coprime and thus number is a member of the group.
+                available_values.append(number)
+            else : continue
+
+        
+        # 2) Now, we'll try every combination of x and y from our pool of available values and check if whether the pair yildes the given instance j
+        for x in available_values:
+            for y in available_values:
+                a = (self.generator**x) % self.modulo
+                b = (self.generator**y) % self.modulo
+                c = (self.generator**(x*y)) % self.modulo
+
+                if(tuple((a,b,c)) == tuple(self.j_instance)):
+                    print(f"j is yes-instance, certificate is: {tuple((x,y))}")
+                    break
+                # else:
+                #     print(f"j {tuple((a,b,c))} is no-instance")
+
+
+
+if __name__ == "__main__":
+    obj = DecisionalDiffieHellman(13,7,(5,9,1))
+
---
+++ Labs/lab_1.py
@@ -0,0 +1,60 @@
+#EX1
+# def ex1 (*vals):
+#     sum = 0
+#     for val in vals:
+#         sum+=val
+    
+#     return sum/len(vals)
+
+# print(ex1(29,32,17,84))
+
+
+
+#EX2
+
+# def addition(a,b):
+#     return a+b
+# def substraction(a,b):
+#     return a-b
+# def multiplication(a,b):
+#     return a*b
+# def division(a,b):
+#     return a/b
+
+# print(addition(3,5))
+# print(substraction(3,5))
+# print(multiplication(3,5))
+# print(division(3,5))
+
+
+#EX3
+
+# def ex3(a,b):
+#     if (a<b):
+#         return -1
+#     elif(a==b):
+#         return 0
+#     else:
+#         return 1
+    
+# print(ex3(3,5))
+
+
+#EX4
+
+# def ex4(a):
+#     for nums in range(1,a):
+#         print(nums)
+
+# ex4(5)
+
+
+#EX5
+
+# def ex5(inp_list):
+#     result = 0
+#     for values in inp_list:
+#         result+=values
+#     return result
+
+# print(ex5([1,2,3,4,5]))
